64e204a407ccbfc9e26f18d4320128216328f55a
denyh6 <denyhaans@gmail.com>
2026-02-03 14:51:00 +0800

    Rename to Wing. Added greeting.

---
+++ src/main/java/Wing.java
@@ -0,0 +1,20 @@
+public class Duke {
+    public static void main(String[] args) {
+        String logo
+                = " __  __  __  ____  __ _  ____   \n"
+                + "\\   /   /  /|_  _||  | ||  __|_ \n"
+                + " \\   /\\   /  _||_ | || || |__  |\n"
+                + "  \\_/  \\_/  |____||_|__||______|\n";
+
+        String StartMsg
+            = """
+                ____________________________________________________________
+                Hello! I'm Wing
+                What can I do for you?
+                ____________________________________________________________
+                Bye. Hope to see you again soon!
+                ____________________________________________________________""";
+
+        System.out.println("Hello from\n" + logo + StartMsg);
+    }
+}
