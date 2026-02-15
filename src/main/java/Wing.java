c97933186520b0bb43bd939a60b19a8969cc6730
denyh6 <denyhaans@gmail.com>
2026-02-15 22:53:46 +0700

    Level-1 Complete Add echo feature in skeletal code

--- src/main/java/Wing.java
+++ src/main/java/Wing.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Wing {
     public static void main(String[] args) {
         String logo
@@ -7,14 +9,25 @@ public class Wing {
                 + "  \\_/  \\_/  |____||_|__||______|\n";
 
         String StartMsg
-            = """
+                = """
                 ____________________________________________________________
                 Hello! I'm Wing
                 What can I do for you?
-                ____________________________________________________________
-                Bye. Hope to see you again soon!
                 ____________________________________________________________""";
 
         System.out.println("Hello from\n" + logo + StartMsg);
+        Scanner in = new Scanner(System.in);
+        boolean isTalking = true;
+        while (isTalking) {
+            String line = in.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("____________________________________________________________\n"
+                        + "Ok. Bye." + "\n____________________________________________________________");
+                isTalking = false;
+                break;
+            }
+            System.out.println("____________________________________________________________\n"
+                    + line + "\n____________________________________________________________");
+        }
     }
 }
