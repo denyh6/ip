c85c8b825036af0d15e8ee7577f5ee45fe12a8fc
denyh6 <denyhaans@gmail.com>
2026-02-16 01:07:47 +0700

    Level-3 complete Add Task.java class Add feature to mark as done or undone Update list feature

--- src/main/java/Wing.java
+++ src/main/java/Wing.java
@@ -8,14 +8,15 @@ public class Wing {
                 + " \\   /\\   /  _||_ | || || |__  |\n"
                 + "  \\_/  \\_/  |____||_|__||______|\n";
 
-        String bar = "____________________________________________________________\n";
+        String bar
+                = "____________________________________________________________\n";
 
         String StartMsg
                 = bar + "Hello! I'm Wing\nWhat can I do for you?\n" + bar;
 
         System.out.println("Hello from\n" + logo + StartMsg);
         Scanner in = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCounter = 0;
         while (true) {
             String line = in.nextLine();
@@ -23,15 +24,25 @@ public class Wing {
                 System.out.println(bar + "Ok. Bye.\n" + bar);
                 break;
             } else if (line.equals("list")) {
-                System.out.print(bar);
+                System.out.print(bar + "Here are the tasks in your list:\n");
                 for (int i = 0; i < taskCounter; i++) {
-                    System.out.println((i + 1) + ": " + tasks[i]);
+                    System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
                 }
                 System.out.print(bar);
+            } else if (line.startsWith("mark")) {
+                tasks[Integer.parseInt(line.substring(5)) - 1].markAsDone();
+                System.out.println(bar + "YAY! I've marked this task as done:\n  ["
+                        + tasks[Integer.parseInt(line.substring(5)) - 1].getStatusIcon() + "] "
+                        + tasks[Integer.parseInt(line.substring(5)) - 1].getDescription() + "\n" + bar);
+            } else if (line.startsWith("unmark")) {
+                tasks[Integer.parseInt(line.substring(7)) - 1].markAsNotDone();
+                System.out.println(bar + "oof sure. I've marked this task as not done yet:\n  ["
+                        + tasks[Integer.parseInt(line.substring(7)) - 1].getStatusIcon() + "] "
+                        + tasks[Integer.parseInt(line.substring(7)) - 1].getDescription() + "\n" + bar);
             } else {
-                tasks[taskCounter] = line;
+                tasks[taskCounter] = new Task(line);
+                System.out.println(bar + "added: " + tasks[taskCounter].getDescription() + "\n" + bar);
                 taskCounter++;
-                System.out.println(bar + "added: " + line + "\n" + bar);
             }
         }
     }
