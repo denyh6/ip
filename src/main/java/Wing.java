61484e870754b2dbfdbd293ef01a4acd16ca4436
denyh6 <denyhaans@gmail.com>
2026-02-25 14:45:15 +0800

    A-CodeQuality complete Refactor code Add methods for each command Update taskCounter int to be a static variable Update code based on TA Github comments on 20/2/2026

--- src/main/java/Wing.java
+++ src/main/java/Wing.java
@@ -1,25 +1,88 @@
 import java.util.Scanner;
 
 public class Wing {
+    private static final int MAX_NUMBER_OF_TASKS = 100;
+    private static int taskCounter = 0;
+    private static final String DIVIDER
+            = "____________________________________________________________\n";
+
+    private static void printBye() {
+        System.out.println(DIVIDER + "Ok. Bye." + System.lineSeparator() + DIVIDER);
+    }
+
+    private static void printList(Task[] tasks) {
+        System.out.print(DIVIDER + "Here are the tasks in your list:" + System.lineSeparator());
+        for (int i = 0; i < taskCounter; i++) {
+            System.out.println((i + 1) + ". " + tasks[i].toString());
+        }
+        System.out.print(DIVIDER);
+    }
+
+    private static void markTask(Task[] tasks, String line){
+        int taskToMark = Integer.parseInt(line.substring(5)) - 1;
+        tasks[taskToMark].markAsDone();
+        System.out.println(DIVIDER + "YAY! I've marked this task as done:\n  ["
+                + tasks[taskToMark].getStatusIcon() + "] "
+                + tasks[taskToMark].getDescription() + System.lineSeparator() + DIVIDER);
+    }
+
+    private static void unmarkTask(Task[] tasks, String line){
+        int taskToUnmark = Integer.parseInt(line.substring(7)) - 1;
+        tasks[taskToUnmark].markAsNotDone();
+        System.out.println(DIVIDER + "oof sure. I've marked this task as not done yet:\n  ["
+                + tasks[taskToUnmark].getStatusIcon() + "] "
+                + tasks[taskToUnmark].getDescription() + System.lineSeparator() + DIVIDER);
+    }
+
+    private static void addTodo(Task[] tasks, String line){
+        String taskTodo = line.substring(5);
+        tasks[taskCounter] = new Todo(taskTodo);
+        System.out.println(DIVIDER + "sigh another todo. I've added this task: " + System.lineSeparator()
+                + tasks[taskCounter].toString() + System.lineSeparator()
+                + "Now you have " + (taskCounter + 1) + " task(s) in the list." + System.lineSeparator() + DIVIDER);
+        taskCounter++;
+    }
+
+    private static void addDeadline(Task[] tasks, String line){
+        int byIndex = line.indexOf("/by");
+        String taskToDeadline = line.substring(9, byIndex - 1);
+        String by = line.substring(byIndex + 4);
+        tasks[taskCounter] = new Deadline(taskToDeadline, by);
+        System.out.println(DIVIDER + "yikes another deadline. I've added this task: " + System.lineSeparator()
+                + tasks[taskCounter].toString() + System.lineSeparator()
+                + "Now you have " + (taskCounter + 1) + " task(s) in the list." + System.lineSeparator() + DIVIDER);
+        taskCounter++;
+    }
+
+    private static void addEvent(Task[] tasks, String line){
+        int startIndex = line.indexOf("/from");
+        String taskToEvent = line.substring(6, startIndex - 1);
+        String startToEndDate = line.substring(startIndex);
+        tasks[taskCounter] = new Event(taskToEvent, startToEndDate);
+        System.out.println(DIVIDER + "GOSH another event. I've added this task: " + System.lineSeparator()
+                + tasks[taskCounter].toString() + System.lineSeparator()
+                + "Now you have " + (taskCounter + 1) + " task(s) in the list." + System.lineSeparator() + DIVIDER);
+        taskCounter++;
+    }
+
+    private static void printCommandError(){
+        System.out.println(DIVIDER + "What talking you?" + System.lineSeparator() + DIVIDER);
+    }
+
     public static void main(String[] args) {
-        String logo
+        String startMessage
                 = """
-                 __  __  __  ____  __ _  ____ \s
+               This is
+                 __  __  __  ____  __ _  ____
                 \\   /   /  /|_  _||  | ||  __|_
                  \\   /\\   /  _||_ | || || |__  |
                   \\_/  \\_/  |____||_|__||______|
-               """;
+               """ + DIVIDER + "Hello! I'm Wing."
+                + System.lineSeparator() + "What can I do for you?" + System.lineSeparator() + DIVIDER;
 
-        String bar
-                = "____________________________________________________________\n";
-
-        String StartMsg
-                = bar + "Hello! I'm Wing\nWhat can I do for you?\n" + bar;
-
-        System.out.println("Hello from" + System.lineSeparator() + logo + StartMsg);
+        System.out.println(startMessage);
         Scanner in = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int taskCounter = 0;
+        Task[] tasks = new Task[MAX_NUMBER_OF_TASKS];
         while (true) {
             String line = in.nextLine().trim();
             int firstSpaceIndex = line.indexOf(" ");
@@ -27,62 +90,43 @@ public class Wing {
 
             switch (firstWord) {
             case "bye":
-                System.out.println(bar + "Ok. Bye." + System.lineSeparator() + bar);
+                printBye();
                 return;
 
             case "list":
-                System.out.print(bar + "Here are the tasks in your list:" + System.lineSeparator());
-                for (int i = 0; i < taskCounter; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i].toString());
-                }
-                System.out.print(bar);
+                printList(tasks);
                 break;
 
             case "mark":
-                int taskToMark = Integer.parseInt(line.substring(5)) - 1;
-                tasks[taskToMark].markAsDone();
-                System.out.println(bar + "YAY! I've marked this task as done:\n  ["
-                        + tasks[taskToMark].getStatusIcon() + "] "
-                        + tasks[taskToMark].getDescription() + System.lineSeparator() + bar);
+                markTask(tasks, line);
                 break;
 
             case "unmark":
-                int taskToUnmark = Integer.parseInt(line.substring(7)) - 1;
-                tasks[taskToUnmark].markAsNotDone();
-                System.out.println(bar + "oof sure. I've marked this task as not done yet:\n  ["
-                        + tasks[taskToUnmark].getStatusIcon() + "] "
-                        + tasks[taskToUnmark].getDescription() + System.lineSeparator() + bar);
+                unmarkTask(tasks, line);
                 break;
 
             case "todo":
-                String taskTodo = line.substring(5);
-                tasks[taskCounter] = new Todo(taskTodo);
-                System.out.println(bar + "sigh another todo. I've added this task: " + System.lineSeparator()
-                        + tasks[taskCounter].toString() + System.lineSeparator()
-                        + "Now you have " + (taskCounter + 1) + " task(s) in the list." + System.lineSeparator() + bar);
-                taskCounter++;
+                addTodo(tasks, line);
                 break;
 
             case "deadline":
-                int byIndex = line.indexOf("/by");
-                String taskToDeadline = line.substring(9, byIndex - 1);
-                String by = line.substring(byIndex + 4);
-                tasks[taskCounter] = new Deadline(taskToDeadline, by);
-                System.out.println(bar + "yikes another deadline. I've added this task: " + System.lineSeparator()
-                        + tasks[taskCounter].toString() + System.lineSeparator()
-                        + "Now you have " + (taskCounter + 1) + " task(s) in the list." + System.lineSeparator() + bar);
-                taskCounter++;
+                if (line.contains("/by")) {
+                    addDeadline(tasks, line);
+                } else {
+                    printCommandError();
+                }
                 break;
 
             case "event":
-                int startIndex = line.indexOf("/from");
-                String taskToEvent = line.substring(6, startIndex - 1);
-                String startToEndDate = line.substring(startIndex);
-                tasks[taskCounter] = new Event(taskToEvent, startToEndDate);
-                System.out.println(bar + "GOSH another event. I've added this task: " + System.lineSeparator()
-                        + tasks[taskCounter].toString() + System.lineSeparator()
-                        + "Now you have " + (taskCounter + 1) + " task(s) in the list." + System.lineSeparator() + bar);
-                taskCounter++;
+                if (line.contains("/from") && line.contains("/to")){
+                    addEvent(tasks, line);
+                } else {
+                    printCommandError();
+                }
+                break;
+
+            default:
+                printCommandError();
                 break;
             }
         }
