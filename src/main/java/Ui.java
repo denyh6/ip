fdd2aa71ba0ae61dd46dae6813ab627039b81f8d
denyh6 <denyhaans@gmail.com>
2026-03-06 21:44:13 +0800

    Add Ui class for A-MoreOOP increment

---
+++ src/main/java/Ui.java
@@ -0,0 +1,90 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ui {
+
+    private static final String DIVIDER
+            = "____________________________________________________________" + System.lineSeparator();
+
+    private static final String ERRORDIVIDER
+            = "============================================================" + System.lineSeparator();
+
+    private static final String startMessage
+            = """
+                This is
+                  __  __  __  ____  __ _  ____
+                 \\   /   /  /|_  _||  | ||  __|_
+                  \\   /\\   /  _||_ | || || |__  |
+                   \\_/  \\_/  |____||_|__||______|
+                """ + DIVIDER + "Hello! I'm Wing."
+            + System.lineSeparator() + "What can I do for you?" + System.lineSeparator() + DIVIDER;
+
+    private final Scanner in;
+
+    public Ui() {
+        this.in = new Scanner(System.in);
+    }
+
+    public void showWelcome() {
+        System.out.println(startMessage);
+    }
+
+    public void showDivider() {
+        System.out.println(DIVIDER);
+    }
+
+    public String getLine() {
+        return in.nextLine();
+    }
+
+    public void showBye() {
+        System.out.println(DIVIDER + "Ok. Bye." + System.lineSeparator() + DIVIDER);
+    }
+
+    public void showAddTask(Task task, int numOfTasks) {
+        System.out.println(DIVIDER + "sigh another task. I've added this task: " + System.lineSeparator()
+                + " " + task.toString() + System.lineSeparator()
+                + "Now there's " + numOfTasks + " task(s) in your list." + System.lineSeparator() + DIVIDER);
+    }
+
+    public void showDeleteTask(Task task, int numOfTasks) {
+        System.out.println(DIVIDER + "Phew! I've removed this task for you:" + System.lineSeparator()
+                + task.toString() + System.lineSeparator()
+                + "Now there's " + (numOfTasks - 1) + " task(s) in your list." + System.lineSeparator() + DIVIDER);
+    }
+
+    public void showMarkTask(Task task) {
+        System.out.println(DIVIDER + "YAY! I've marked this task as done:" + System.lineSeparator()
+                + task.toString() + System.lineSeparator() + DIVIDER);
+    }
+
+    public void showUnmarkTask(Task task) {
+        System.out.println(DIVIDER + "ok sure. This task is unmarked:" + System.lineSeparator()
+                + task.toString() + System.lineSeparator() + DIVIDER);
+    }
+
+    public void showList(ArrayList<Task> tasks) {
+        System.out.print(DIVIDER + "Here's your list:" + System.lineSeparator());
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(" " + (i + 1) + ". " + tasks.get(i).toString());
+        }
+        System.out.print(DIVIDER);
+    }
+
+    public void showInvalidCommand() {
+        System.out.println(DIVIDER + "EH! What talking you?" + System.lineSeparator() + DIVIDER);
+    }
+
+    public void showError(String error) {
+        System.out.println(ERRORDIVIDER + error + System.lineSeparator() + ERRORDIVIDER);
+    }
+
+    public void showSavedList(Scanner scanFile) {
+        System.out.println("Here's your previous saved list btw");
+        while (scanFile.hasNext()) {
+            System.out.println(scanFile.nextLine());
+        }
+        System.out.println(System.lineSeparator());
+    }
+
+}
--- src/main/java/Wing.java
+++ src/main/java/Wing.java
@@ -6,22 +6,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Wing {
-    private static final int MAX_NUMBER_OF_TASKS = 100;
-    private static int taskCounter = 0;
-    private static final String DIVIDER
-            = "____________________________________________________________\n";
 
-    private static void printBye() {
-        System.out.println(DIVIDER + "Ok. Bye." + System.lineSeparator() + DIVIDER);
-    }
-
-    private static void printList(ArrayList<Task> tasks) {
-        System.out.print(DIVIDER + "Here's your list:" + System.lineSeparator());
-        for (int i = 0; i < tasks.size(); i++) {
-            System.out.println((i + 1) + ". " + tasks.get(i).toString());
-        }
-        System.out.print(DIVIDER);
-    }
+    private static Ui ui;
 
     private static void markTask(ArrayList<Task> tasks, String line) throws NoSuchTaskException {
         int taskToMark = Integer.parseInt(line.substring(5)) - 1;
@@ -29,8 +15,7 @@ public class Wing {
             throw new NoSuchTaskException();
         }
         tasks.get(taskToMark).markAsDone();
-        System.out.println(DIVIDER + "YAY! I've marked this task as done:" + System.lineSeparator()
-                + tasks.get(taskToMark).toString() + System.lineSeparator() + DIVIDER);
+        ui.showMarkTask(tasks.get(taskToMark));
     }
 
     private static void unmarkTask(ArrayList<Task> tasks, String line) throws NoSuchTaskException {
@@ -39,8 +24,7 @@ public class Wing {
             throw new NoSuchTaskException();
         }
         tasks.get(taskToUnmark).markAsNotDone();
-        System.out.println(DIVIDER + "oof sure. I've marked this task as not done yet:" + System.lineSeparator()
-                + tasks.get(taskToUnmark).toString() + System.lineSeparator() + DIVIDER);
+        ui.showUnmarkTask(tasks.get(taskToUnmark));
     }
 
     private static void addTodo(ArrayList<Task> tasks, String line) throws NoDescriptionException {
@@ -50,9 +34,7 @@ public class Wing {
         String taskTodo = line.substring(5);
         Task newTask = new Todo(taskTodo);
         tasks.add(newTask);
-        System.out.println(DIVIDER + "sigh another todo. I've added this task: " + System.lineSeparator()
-                + tasks.get(tasks.indexOf(newTask)).toString() + System.lineSeparator()
-                + "Now there's " + tasks.size() + " task(s) in your list." + System.lineSeparator() + DIVIDER);
+        ui.showAddTask(newTask, tasks.size());
     }
 
     private static void addDeadline(ArrayList<Task> tasks, String line) throws NoByException, NoDescriptionException {
@@ -67,9 +49,7 @@ public class Wing {
         String by = line.substring(byIndex + 4);
         Deadline newDeadline = new Deadline(taskToDeadline, by);
         tasks.add(newDeadline);
-        System.out.println(DIVIDER + "yikes another deadline. I've added this task: " + System.lineSeparator()
-                + tasks.get(tasks.indexOf(newDeadline)).toString() + System.lineSeparator()
-                + "Now there's " + tasks.size() + " task(s) in your list." + System.lineSeparator() + DIVIDER);
+        ui.showAddTask(newDeadline, tasks.size());
     }
 
     private static void addEvent(ArrayList<Task> tasks, String line)
@@ -88,9 +68,7 @@ public class Wing {
         String startToEndDate = line.substring(startIndex);
         Event newEvent = new Event(taskToEvent, startToEndDate);
         tasks.add(newEvent);
-        System.out.println(DIVIDER + "GOSH another event. I've added this task: " + System.lineSeparator()
-                + tasks.get(tasks.indexOf(newEvent)).toString() + System.lineSeparator()
-                + "Now there's " + tasks.size() + " task(s) in your list." + System.lineSeparator() + DIVIDER);
+        ui.showAddTask(newEvent, tasks.size());
     }
 
     private static void deleteTask(ArrayList<Task> tasks, String line) throws NoSuchTaskException {
@@ -98,9 +76,7 @@ public class Wing {
         if (taskToDelete < 0 || taskToDelete >= tasks.size()) {
             throw new NoSuchTaskException();
         }
-        System.out.println(DIVIDER + "Phew! I've removed this task for you:" + System.lineSeparator()
-                + tasks.get(taskToDelete).toString() + System.lineSeparator()
-                + "Now there's " + (tasks.size() - 1) + " task(s) in your list." + System.lineSeparator() + DIVIDER);
+        ui.showDeleteTask(tasks.get(taskToDelete), tasks.size());
         tasks.remove(taskToDelete);
     }
 
@@ -115,11 +91,7 @@ public class Wing {
             return;
         }
         Scanner scanFile = new Scanner(file);
-        System.out.println("Here's your previous saved list btw");
-        while (scanFile.hasNext()) {
-            System.out.println(scanFile.nextLine());
-        }
-        System.out.println(System.lineSeparator());
+        ui.showSavedList(scanFile);
     }
 
     private static void updateFile(ArrayList<Task> tasks) throws IOException {
@@ -134,38 +106,31 @@ public class Wing {
     }
 
     public static void main(String[] args) {
-        String startMessage
-                = """
-                This is
-                  __  __  __  ____  __ _  ____
-                 \\   /   /  /|_  _||  | ||  __|_
-                  \\   /\\   /  _||_ | || || |__  |
-                   \\_/  \\_/  |____||_|__||______|
-                """ + DIVIDER + "Hello! I'm Wing."
-                + System.lineSeparator() + "What can I do for you?" + System.lineSeparator() + DIVIDER;
 
-        System.out.println(startMessage);
+        ui = new Ui();
+        ui.showWelcome();
         try {
             printFileContents();
         } catch (FileNotFoundException e) {
-            System.out.println("File not found!");
+            ui.showError("File not found!");
         } catch (IOException e) {
-            System.out.println("Error printing: " + e.getMessage());
+            ui.showError("Error printing: " + e.getMessage());
         }
-        Scanner in = new Scanner(System.in);
+
         ArrayList<Task> tasks = new ArrayList<>();
+
         while (true) {
-            String line = in.nextLine().trim();
+            String line = ui.getLine().trim();
             int firstSpaceIndex = line.indexOf(" ");
             String firstWord = (firstSpaceIndex > -1) ? line.substring(0, firstSpaceIndex) : line;
 
             switch (firstWord) {
             case "bye":
-                printBye();
+                ui.showBye();
                 return;
 
             case "list":
-                printList(tasks);
+                ui.showList(tasks);
                 break;
 
             case "mark":
@@ -173,9 +138,9 @@ public class Wing {
                     markTask(tasks, line);
                     updateFile(tasks);
                 } catch (NoSuchTaskException e) {
-                    System.out.println(DIVIDER + "EH! No such task!" + System.lineSeparator() + DIVIDER);
+                    ui.showError("EH! No such task!");
                 } catch (IOException e) {
-                    System.out.println("Error updating: " + e.getMessage());
+                    ui.showError("Error updating: " + e.getMessage());
                 }
                 break;
 
@@ -184,9 +149,9 @@ public class Wing {
                     unmarkTask(tasks, line);
                     updateFile(tasks);
                 } catch (NoSuchTaskException e) {
-                    System.out.println(DIVIDER + "EH! No such task!" + System.lineSeparator() + DIVIDER);
+                    ui.showError("EH! No such task!");
                 } catch (IOException e) {
-                    System.out.println("Error updating: " + e.getMessage());
+                    ui.showError("Error updating: " + e.getMessage());
                 }
                 break;
 
@@ -195,9 +160,9 @@ public class Wing {
                     addTodo(tasks, line);
                     updateFile(tasks);
                 } catch (NoDescriptionException e) {
-                    System.out.println(DIVIDER + "EH! Forgot your description!" + System.lineSeparator() + DIVIDER);
+                    ui.showError("EH! Forgot your description!");
                 } catch (IOException e) {
-                    System.out.println("Error updating: " + e.getMessage());
+                    ui.showError("Error updating: " + e.getMessage());
                 }
                 break;
 
@@ -206,11 +171,11 @@ public class Wing {
                     addDeadline(tasks, line);
                     updateFile(tasks);
                 } catch (NoByException e) {
-                    System.out.println(DIVIDER + "EH! Forgot your /by deadline!" + System.lineSeparator() + DIVIDER);
+                    ui.showError("EH! Forgot your /by deadline!");
                 } catch (NoDescriptionException e) {
-                    System.out.println(DIVIDER + "EH! Forgot your description!" + System.lineSeparator() + DIVIDER);
+                    ui.showError("EH! Forgot your description!");
                 } catch (IOException e) {
-                    System.out.println("Error updating: " + e.getMessage());
+                    ui.showError("Error updating: " + e.getMessage());
                 }
                 break;
 
@@ -219,13 +184,13 @@ public class Wing {
                     addEvent(tasks, line);
                     updateFile(tasks);
                 } catch (NoFromException e) {
-                    System.out.println(DIVIDER + "EH! Forgot your /from date!" + System.lineSeparator() + DIVIDER);
+                    ui.showError("EH! Forgot your /from date!");
                 } catch (NoToException e) {
-                    System.out.println(DIVIDER + "EH! Forgot your /to date!" + System.lineSeparator() + DIVIDER);
+                    ui.showError("EH! Forgot your /to date!");
                 } catch (NoDescriptionException e) {
-                    System.out.println(DIVIDER + "EH! Forgot your description!" + System.lineSeparator() + DIVIDER);
+                    ui.showError("EH! Forgot your description!");
                 } catch (IOException e) {
-                    System.out.println("Error updating: " + e.getMessage());
+                    ui.showError("Error updating: " + e.getMessage());
                 }
                 break;
 
@@ -234,14 +199,14 @@ public class Wing {
                     deleteTask(tasks, line);
                     updateFile(tasks);
                 } catch (NoSuchTaskException e) {
-                    System.out.println(DIVIDER + "EH! No such task!" + System.lineSeparator() + DIVIDER);
+                    ui.showError("EH! No such task!");
                 } catch (IOException e) {
-                    System.out.println("Error updating: " + e.getMessage());
+                    ui.showError("Error updating: " + e.getMessage());
                 }
                 break;
 
             default:
-                System.out.println(DIVIDER + "EH! What talking you?" + System.lineSeparator() + DIVIDER);
+                ui.showInvalidCommand();
                 break;
             }
         }
